ba7c7ae6db0451be55a16af62e01e6bc7a99658c
Mateo Prestia <prestia.mateo@gmail.com>
2025-07-04 14:16:47 -0300

    Initial commit

---
+++ main/institucional/Autoridades/header.php
@@ -0,0 +1,23 @@
+<?php include 'head.php'; ?>
+
+<body>
+<header>
+    <nav class="navbar">
+    <div class="navbar-logo">
+      <img src="logo_viejo.png" alt="Logo Defensoría" />
+    </div>
+    <div class="navbar-menu">
+      <div class="dropdown">
+        <a href="#">Institucional</a>
+        <div class="dropdown-content">
+          <a href="#">¿Qué hacemos?</a>
+          <a href="#">Autoridades</a>
+        </div>
+      </div>
+      <a href="#">Prensa</a>
+      <a href="#">Normativa</a>
+      <a href="#">Links</a>
+      <a href="#">Contáctenos</a>
+    </div>
+  </nav>
+</header>
---
+++ main/institucional/Que_Hacemos/footer.php
@@ -0,0 +1,3 @@
+<footer>
+    <p>© 2025 Defensoría del Pueblo - Todos los derechos reservados</p>
+</footer>

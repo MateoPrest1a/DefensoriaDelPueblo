5c77eac979aafca59959d518ed3612375c10191d
Mateo <prestia.mateo@gmail.com>
2025-08-08 11:25:26 -0300

    Cambios en Seccion Autoridades BackOffice
    Agregado de archivos de parametros y cabecera

--- BackOffice/Main/Secciones/autoridades.php
+++ BackOffice/Main/Secciones/autoridades.php
@@ -1,9 +1,16 @@
+<?php 
+
+    include "../../../parametros.php";
+    include "../../../plantilla/head.php";
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title></title>
 </head>
 <body>
     

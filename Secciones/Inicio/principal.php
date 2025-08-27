7c0077ed7ef83a48b5ea9a1f0c4d975a6748d72a
Camila Ernaga <camilliben03@gmail.com>
2025-08-27 13:15:35 -0300

    arreglos

--- Secciones/Inicio/principal.php
+++ Secciones/Inicio/principal.php
@@ -5,7 +5,11 @@
 <body>
   <section class="hero">
     <h1>Defensoría del Pueblo</h1>
-    <p>Protegiendo tus derechos, escuchando tu voz. Conectate con nosotros para que tus inquietudes sean escuchadas.</p>
+    <p>Protegiendo tus derechos, escuchando tu voz. Conectate con nosotros para que tus inquietudes sean escuchadas.
+      Esta es la pagina web de la Defensoría del Pueblo de la Municipalidad de General Pueyrredon.
+      Por este medio trataremos de dar a conocer nuestro trabajo, las acciones que realizamos y el tratamiento dado a los temas que nos llegan.
+      Creemos en un sistema comunicacional de ida y vuelta, por lo que sumamos este medio para que se contacten con nosotros para acercarnos las inquietudes y quejas para poder analizarlas y tratarlas.
+    </p>
   </section>
 
   <section class="card-section">
@@ -22,9 +26,13 @@
       <p>Mandanos tus inquietudes, quejas o sugerencias. Estamos para escucharte.</p>
     </div>
   </section>
-
-  <?php include('../../plantilla/footer.php'); ?>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+  <h2 class="text-center mb-4">¿Dónde estamos?</h2>
+  <div class="map-container mx-auto text-center" style="max-width: 800px;">
+    <a href="https://www.google.com.ar/maps/place/Municipalidad+de+General+Pueyrredon/@-37.9976633,-57.5516251,16z/data=!3m1!4b1!4m6!3m5!1s0x9584dc03bb58eb2b:0xfc58db7e5aa0879f!8m2!3d-37.9976663!4d-57.5498044!16s%2Fg%2F1tdb0w9g?hl=es&entry=ttu&g_ep=EgoyMDI1MDcwOS4wIKXMDSoASAFQAw%3D%3D" target="_blank">
+      <img src="./images/mapa.png" alt="Ubicación Municipalidad" class="img-fluid" style="max-height: 400px; object-fit: cover;">
+    </a>
+   
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+<?php include('../../plantilla/footer.php'); ?>
 </html>
-

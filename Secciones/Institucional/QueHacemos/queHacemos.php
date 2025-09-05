772d99d1e8a93108ebe04f6fe18f39bc4bd05456
Mateo <prestia.mateo@gmail.com>
2025-09-05 14:54:14 -0300

    Arreglos generales para el funcionamiento del FrontEnd

--- Secciones/Institucional/QueHacemos/queHacemos.php
+++ Secciones/Institucional/QueHacemos/queHacemos.php
@@ -1,6 +1,4 @@
-<?php include "../../../plantilla/head.php"; 
-?>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+<?php include "../../../plantilla/head.php"; ?>
 
 <?php
     include_once __DIR__ . '/../../../breadcrumbConfig.php';

2dbc9f31fbe96831fcceb727fb95c0f11c45dd19
Mateo <prestia.mateo@gmail.com>
2025-09-08 13:49:54 -0300

    Cambios en QueHacemos, uso de archivo Style.css
    No son finales, hay que hacer una revision de eso

--- Secciones/Inicio/principal.php
+++ Secciones/Inicio/principal.php
@@ -28,7 +28,7 @@
     </div>
   </section>
   <h2 class="text-center mb-4">¿Dónde estamos?</h2>
-  <div class="map-container mx-auto text-center" style="max-width: 800px;">
+  <div class="map-container mx-auto text-center imagen-cuadro" style="max-width: 800px;">
     <a href="https://www.google.com.ar/maps/place/Municipalidad+de+General+Pueyrredon/@-37.9976633,-57.5516251,16z/data=!3m1!4b1!4m6!3m5!1s0x9584dc03bb58eb2b:0xfc58db7e5aa0879f!8m2!3d-37.9976663!4d-57.5498044!16s%2Fg%2F1tdb0w9g?hl=es&entry=ttu&g_ep=EgoyMDI1MDcwOS4wIKXMDSoASAFQAw%3D%3D" target="_blank">
       <img src="./images/mapa.png" alt="Ubicación Municipalidad" class="img-fluid" style="max-height: 400px; object-fit: cover;">
     </a>
--- Secciones/Institucional/QueHacemos/queHacemos.php
+++ Secciones/Institucional/QueHacemos/queHacemos.php
@@ -5,15 +5,26 @@
     include_once __DIR__ . '/../../../breadcrumbConfig.php';
     include_once __DIR__ . '/../../../breadcrumb.php';
 ?>
-          <div class="row align-items-center py-3">
+          <div class="row align-items-center py-3">                                
                 <div class="col-12 col-md-5 text-center">
-                    <img src="images/logo_viejo.png" class="img-shadow img-fluid" style="max-width: 300px;">
+                    <!--<img src="images/logo_viejo.png" class="img-shadow img-fluid" style="max-width: 300px;">-->
+                    <div class="contenedor-roller">
+                        <h1 class="roller-text">Somos</h1>
+                            <div class="roller">
+                                <div class="roller-inner">
+                                    <span>Con</span>
+                                    <span>y Para</span>
+                                </div>
+                            </div>
+                        <h1 class="roller-text">El Pueblo</h1>
+                    </div>
                 </div>
+
                 <div class="col-12 col-md-7">
-                    <div class="card-extension">
-                        <div class="card-body">
-                            <h1 class="titulo-seccion">¿Qué hacemos?</h1>
-                            <p>Debe consignarse que la operatoria de la Defensoría del Pueblo se rige por la Ordenanza 13663 y modificatorias que, en su artículo 15º, establece que su actuación no estará sujeta a formalidad alguna; en virtud de ello, el procedimiento implementado permite – en una primera atención – la evaluación respecto a la incumbencia del tema traído a consulta y, en caso de no corresponder la intervención, su derivación al organismo competente. Evaluado el tema y, de resultar pertinente al ámbito de actuación de la Defensoría, hay distintos procedimientos, dos de ellos previos a la formalización de una actuación, que se llevan a cabo: Si el ciudadano presentante no ha efectuado previamente su queja a la dependencia municipal directamente involucrada y/o a la empresa prestadora de servicios público, se le solicita que cumplimente con ello y – de no mediar respuesta favorable – vuelva a la Defensoría para iniciar la actuación correspondiente. Si el caso traído a consulta requiere de una respuesta rápida y permite un tipo de gestión directa se procede a viabilizar tal procedimiento a través de la intervención telefónica o derivación directa a la persona que podrá dar solución al mismo, con resultado positivo desde el propio ciudadano y la administración, dado el ahorro de costo en tiempo y dinero que demanda la puesta en marcha de los mecanismos burocráticos tradicionales de la administración pública. Finalmente, y cuando ninguno de los procedimientos precedentes han podido instrumentarse, se inicia la tramitación de un expediente (Actuación) que opera a partir de las normas y procedimientos administrativos previstos en la Ordenanza de creación y la Ordenanza General de Procedimientos Administrativos.
+                    <div class="card-extension anim-container">
+                        <div class="card-body anim-slide">
+                            <h1 class="titulo-seccion anim-fade-titulo">¿Qué hacemos?</h1>
+                            <p class="anim-fade-text">Debe consignarse que la operatoria de la Defensoría del Pueblo se rige por la Ordenanza 13663 y modificatorias que, en su artículo 15º, establece que su actuación no estará sujeta a formalidad alguna; en virtud de ello, el procedimiento implementado permite – en una primera atención – la evaluación respecto a la incumbencia del tema traído a consulta y, en caso de no corresponder la intervención, su derivación al organismo competente. Evaluado el tema y, de resultar pertinente al ámbito de actuación de la Defensoría, hay distintos procedimientos, dos de ellos previos a la formalización de una actuación, que se llevan a cabo: Si el ciudadano presentante no ha efectuado previamente su queja a la dependencia municipal directamente involucrada y/o a la empresa prestadora de servicios público, se le solicita que cumplimente con ello y – de no mediar respuesta favorable – vuelva a la Defensoría para iniciar la actuación correspondiente. Si el caso traído a consulta requiere de una respuesta rápida y permite un tipo de gestión directa se procede a viabilizar tal procedimiento a través de la intervención telefónica o derivación directa a la persona que podrá dar solución al mismo, con resultado positivo desde el propio ciudadano y la administración, dado el ahorro de costo en tiempo y dinero que demanda la puesta en marcha de los mecanismos burocráticos tradicionales de la administración pública. Finalmente, y cuando ninguno de los procedimientos precedentes han podido instrumentarse, se inicia la tramitación de un expediente (Actuación) que opera a partir de las normas y procedimientos administrativos previstos en la Ordenanza de creación y la Ordenanza General de Procedimientos Administrativos.
                         </div>
                     </div>
                 </div>

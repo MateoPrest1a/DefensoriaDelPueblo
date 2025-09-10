a80d9330084852c59cbbe65138c241fa4dbb0c25
Camila Ernaga <camilliben03@gmail.com>
2025-09-10 14:18:43 -0300

    arreglos de logo

--- plantilla/footer.php
+++ plantilla/footer.php
@@ -9,7 +9,7 @@
 
           <!-- Logo -->
           <div class="col-md-4 mb-4 mb-md-0">
-            <img src="<?= BASE_URL ?>plantilla/logo_viejo.png" style="height: 60px;" class="mb-3">
+            <img src="<?= BASE_URL ?>plantilla/logo_nuevo.jpeg" style="height: 100px; border-radius:50%" class="mb-3">
             <h5 class="fw-bold">Defensoría del Pueblo</h5>
             <p class="mb-0">Partido de General Pueyrredon</p>
           </div>

c9b2dc651ad1491e410cfc45e946ca3abd557292
Juan Cruz Manzo <juancruzmanzo9@gmail.com>
2025-09-22 11:33:11 -0300

    utf solucionado en noticia

--- Secciones/Prensa/noticias/detalleNoticia.php
+++ Secciones/Prensa/noticias/detalleNoticia.php
@@ -3,6 +3,7 @@ include('../../../plantilla/head.php');
 include_once __DIR__ . "/../../../Conexion/conexion.php";
 include_once __DIR__ . '/../../../breadcrumbConfig.php'; 
 include_once __DIR__ . '/../../../breadcrumb.php';
+mysqli_set_charset($link, "utf8mb4");
 
 $id = isset($_GET['id']) ? intval($_GET['id']) : 0;
 

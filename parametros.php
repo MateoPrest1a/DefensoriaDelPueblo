5fbb5e6ad4a6a670ee9b8ed207eaa0deb4146e83
Juan Cruz Manzo <juancruzmanzo9@gmail.com>
2025-09-24 12:54:26 -0300

    modificacion en el footer y agregado de parametros para los defines

--- parametros.php
+++ parametros.php
@@ -16,6 +16,19 @@
 
     define("URL_ORDENANZAS", BASE_URL . "Secciones/Normativa/Ordenanzas/");
     define("URL_RESOLUCION", BASE_URL . "Secciones/Normativa/Resolucion/");
+
+    define("URL_REGLAMENTO", BASE_URL . "Secciones/Normativa/Reglamento/");
+    define("URL_QUEHACEMOS", BASE_URL . "Secciones/Institucional/QueHacemos/");
+    define("URL_AUTORIDADES", BASE_URL . "Secciones/Institucional/Autoridades/");
+    define("URL_NOTICIAS", BASE_URL . "Secciones/Prensa/Noticias/");
+    define("URL_MEMORIAS", BASE_URL . "Secciones/Prensa/Memorias/");
+    define("URL_FAQ", BASE_URL . "Secciones/Contactenos/FAQ/");
+    define("URL_CONTACTENOS", BASE_URL . "Secciones/Contactenos/FormContacto/");
+    define("URL_RESENAS", BASE_URL . "Secciones/Contactenos/Resenas/");
+    define("URL_LINKS", BASE_URL . "Secciones/Links/");
+    define("URL_PRINCIPAL", BASE_URL . "Secciones/Inicio/principal.php");
+    define("URL_LOGIN", BASE_URL . "Secciones/Login/login.php");
+    define("URL_LOGOUT", BASE_URL . "Secciones/Login/logout.php");    
     session_start();
     function archivo_actual(){
         return basename($_SERVER['PHP_SELF']);

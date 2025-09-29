4e0aa20eddd02e92920d78b3d299a925f294b9e6
Mateo <prestia.mateo@gmail.com>
2025-09-29 12:22:10 -0300

    Cambio de nombre en item de navbar Reseña

--- plantilla/head.php
+++ plantilla/head.php
@@ -95,7 +95,7 @@ $archivo = archivo_actual();
           <ul class="dropdown-menu dropdown-menu-end">
             <li><a class="dropdown-item" href="<?= BASE_URL ?>Secciones/Contactenos/FAQ/faq.php">Preguntas Frecuentes</a></li>
             <li><a class="dropdown-item" href="<?= BASE_URL ?>Secciones/Contactenos/FormContacto/contactenos.php">Formulario de Contacto</a></li>
-            <li><a class="dropdown-item" href="<?= BASE_URL ?>Secciones/Contactenos/Resenas/resenas.php">Reseña (BETA)</a></li>
+            <li><a class="dropdown-item" href="<?= BASE_URL ?>Secciones/Contactenos/Resenas/resenas.php">Reseña</a></li>
           </ul>
         </li>
       </ul>
